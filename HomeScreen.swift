c84aef46c7f7243ef5bcb07fecfa45c6ef5cdb14
Samuel L <samasherlefcourt@aol.com>
2019-04-05 23:08:04 -0500

    Revert "merge"

---
+++ HomeScreen.swift
@@ -0,0 +1,10 @@
+//
+//  HomeScreen.swift
+//  QRCodeReader
+//
+//  Created by Samuel Lefcourt on 2/1/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import Foundation
+
--- QRCodeReader/AppDelegate.swift
+++ QRCodeReader/AppDelegate.swift
@@ -15,8 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        STPPaymentConfiguration.shared().publishableKey = "pk_test_TYooMQauvdEDq54NiTphI7jx"
-       return true
         return true
     }
 
@@ -44,3 +42,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+
