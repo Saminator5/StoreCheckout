331988fa9279ff00a557a3bff0cf4239cb8b4399
Samuel L <samasherlefcourt@aol.com>
2019-04-05 23:25:57 -0500

    Revert "Revert "merge""

--- QRCodeReader/AppDelegate.swift
+++ QRCodeReader/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        STPPaymentConfiguration.shared().publishableKey = "pk_test_TYooMQauvdEDq54NiTphI7jx"
+       return true
         return true
     }
 
@@ -42,4 +44,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
