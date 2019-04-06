e918682d6bd177fd78a76f7113d91ec03b4effa0
John Doe <slefcourt12@gmail.com>
2019-04-05 23:24:05 -0500

    repushing

--- QRCodeReader/AppDelegate.swift
+++ QRCodeReader/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         STPPaymentConfiguration.shared().publishableKey = "pk_test_TYooMQauvdEDq54NiTphI7jx"
-       return true
         return true
     }
 
